0b09b6f1bc6462b7239986dc26bf80bfb5fcacac
Baranoff Ievgen <baranoffei@gmail.com>
2017-11-07 15:34:24 +0200

    Template component ContentChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,12 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { ChildComponent } from './child.component';
 
 @Component({
   selector: 'my-app',
-  template: `<child-comp></child-comp>
-            <button (click)="increment()">+</button>
-            <button (click)="decrement()">-</button>
-  `
+  template: `<child-comp>
+                <h3 #headerContent>Добро пожаловать {{name}}!</h3>
+            </child-comp>`
 })
 export class AppComponent {
-  name:string='Tom';
-
-  @ViewChild(ChildComponent)
-  private counterCompomemt: ChildComponent;
-
-  increment() {this.counterCompomemt.increment(); }
-  decrement() {this.counterCompomemt.decrement(); }
+  name: string = "Baer";
 }
