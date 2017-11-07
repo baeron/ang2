ff84b74f6aa9b334fa26ab32212f299b2160f94a
Baranoff Ievgen <baranoffei@gmail.com>
2017-11-07 13:23:11 +0200

    two way binding from parent to child (todo add time to this question)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h2>Количество кликов: {{clicks}}</h2>
-            <second-comp (onChanged)="onChanged($event)"></second-comp>
-            <child-comp [userName]="name" [userAge]="age"></child-comp>
-            <input type="number" [(ngModel)]="age" />`
+  template: `<h2>Количество кликов: {{clicks}}</h2>`+
+            //<second-comp (onChanged)="onChanged($event)"></second-comp>
+            //<child-comp [userName]="name" [userAge]="age"></child-comp>
+            //<input type="number" [(ngModel)]="age" />
+            `<third-child-comp [(userName)]="name"></third-child-comp>
+            <div>Выбранное имя: {{name}}</div>`
 })
 export class AppComponent {
+//  newName:string = "Baer";
   name:string='Tom';
   age:number=24;
   clicks:number = 0;
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child.component'
 import { SecondChildComponent } from './secondChild.component';
+import { ThirdChildComponent } from './thirdChild.component';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ChildComponent, SecondChildComponent ],
+  declarations: [ AppComponent, ChildComponent, SecondChildComponent, ThirdChildComponent ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
