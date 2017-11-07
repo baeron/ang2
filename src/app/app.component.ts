07d19a2683618f4a69016424852a47d9d2716805
Baranoff Ievgen <baranoffei@gmail.com>
2017-11-08 00:44:06 +0200

    Get props in directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { Component} from '@angular/core';
     selector: 'my-app',
     template: `<div>
                 <p bold>Hello Angular 5</p>
-                <p italic>God bay Angular 2</p>
+                <p [italic]="'28px'" [defaultSize]="'14px'">God bay Angular 2</p>
                 <p underline> Angular JS is dead </p>
               </div>`
 })
--- src/app/italic.directive.ts
+++ src/app/italic.directive.ts
@@ -1,22 +1,40 @@
-import { Directive, ElementRef, Renderer2, HostListener, HostBinding} from '@angular/core';
+import { Directive, Input,  HostListener, HostBinding, OnInit} from '@angular/core';
 
 @Directive({
   selector: '[italic]'
 })
 
-export class ItalicDirective{
-    private fontStyle = "normal";
-  constructor(){}
-  @HostBinding("style.fontStyle") get getFontStyle() {
+export class ItalicDirective implements OnInit{
+  @Input('italic') selectedSize = "18px";
+  @Input() defaultSize = "16px";
+
+  private fontSize : string;
+  private fontStyle = "normal";
+
+  ngOnInit(): void {
+    this.fontSize = this.defaultSize;
+  }
+
+  constructor(){ }
+
+  @HostBinding("style.fontSize") get getFontSize(){
+    return this.fontSize;
+  }
+
+  @HostBinding("style.fontStyle") get getFontStyle(){
     return this.fontStyle;
   }
+
   @HostBinding("style.cursor") get getCursor(){
     return "pointer";
   }
-  @HostListener("mouseenter") onMouseEnter() {
-    this.fontStyle = "italic";
+
+  @HostListener("mouseenter") onMouseEnter(){
+      this.fontStyle = "italic";
+      this.fontSize = this.selectedSize;
   }
-  @HostListener("mouseleave") onMouseLeave() {
+  @HostListener("mouseleave") onMouseLeave(){
     this.fontStyle = "normal";
-  }
+    this.fontSize = this.defaultSize;
+}
 }
