75d3676ee24373dc07dd276f2534ef6bd3a87a74
Baranoff Ievgen <baranoffei@gmail.com>
2017-11-07 16:09:35 +0200

    ngClass example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
-import { ChildComponent } from './child.component';
+import { Component} from '@angular/core';
 
 @Component({
-  selector: 'my-app',
-  template: `<child-comp>
-                <h3 #headerContent>Добро пожаловать {{name}}!</h3>
-            </child-comp>`
+selector: 'my-app',
+template: `<div [ngClass]="{verdanaFont:true}">
+             <h1>Hello Angular 2</h1>
+             <p [ngClass]="{verdanaFont:false, segoePrintFont:true}">
+                 Angular 5 представляет модульную архитектуру приложения
+             </p>
+         </div>`,
+styles: [
+ `.verdanaFont{font-size:13px; font-family:Verdana;}
+ .segoePrintFont{font-size:14px; font-family:"Segoe Print";}`
+]
 })
-export class AppComponent {
-  name: string = "Baer";
-}
+export class AppComponent { }
