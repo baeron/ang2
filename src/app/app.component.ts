bca7ed3785545821c02d14b79228d11e45e1a0e1
Baranoff Ievgen <baranoffei@gmail.com>
2017-11-08 14:10:07 +0200

    Module FormsModule and the NgModel directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from './data.service';
-import { LogService } from './log.service';
-import { Phone } from './phone';
-       
+
+export class Phone{
+  constructor(
+    public title:string, 
+    public price: number, 
+    public company: string)
+  {}
+}
+
 @Component({
     selector: 'my-app',
-    template: `<data-comp></data-comp>
-              <data-comp></data-comp>`
+    template: `<div class="col-xs-10">
+                <div class="form-group">
+                  <label>Model name</label>
+                  <input class="form-control" name="title" [(ngModel)]="phone.title" />
+                </div>
+                <div class="form-group">
+                  <label>Price</label>
+                  <input type="number" class="form-control" name="price" [(ngModel)]="phone.price" />
+                </div>
+                <div class="form-group">
+                  <label>Company</label>
+                  <select class="form-control" name="company" [(ngModel)]="phone.company">
+                    <option *ngFor="let comp of companies" [value]="comp">
+                      {{comp}}
+                    </option>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <button class="btn btn-default"(click)="addPhone(title, price, company)">
+                    Add
+                  </button>
+                </div>
+              </div>
+              <div>
+                <h3>Added elements</h3>
+                <ul *ngFor="let p of phones">
+                  <li>{{p.title}} ({{p.company}})-{{p.price}}</li>
+                </ul>
+              </div>`
 })
-export class AppComponent {}
+export class AppComponent {
+  phone: Phone = new Phone("", 0, "Apple");
+  phones: Phone[] = [];
+  companies: string[] = ["Apple", "Xiaomi", "Samsung", "LG"];
+
+  addPhone(){
+    this.phones.push(new Phone(this.phone.title, this.phone.price, this.phone.company));
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { DataComponent } from './data.component';
-import { DataService } from './data.service';
-import { LogService } from './log.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, DataComponent ],
-  providers: [DataService, LogService],
+  declarations: [ AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
