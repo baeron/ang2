9aebd4ab140e459c3a9da39c5c4f80df922176e2
Baranoff Ievgen <baranoffei@gmail.com>
2017-11-08 13:19:19 +0200

    One service for all components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,47 +5,7 @@ import { Phone } from './phone';
        
 @Component({
     selector: 'my-app',
-    template: `<div class="panel">
-                <div class="form-inline">
-                  <div class="form-group">
-                    <div class="col-md-7">
-                      <input class="form-control" [(ngModel)]="name" placeholder="Модель"/>
-                    </div>
-                  </div>
-                  <div class="form-group">
-                  <div class="col-md-5">
-                    <input type="number" class="form-control" [(ngModel)]="price" placeholder="Цена"/>
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="col-md-offset-3 col-md-9">
-                    <button class="btn btn-default" (click)="addItem(name, price)">Добавить</button>
-                  </div>
-                </div>
-                <table class="table table-striped">
-                  <thead>
-                    <tr>
-                      <th>Модель</th>
-                      <th>Цена</th>
-                    </tr>
-                  </thead>
-                <tbody>
-                  <tr *ngFor="let item of items">
-                    <td>{{item.name}}</td>
-                    <td>{{item.price}}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>`,
-            providers: [DataService, LogService]
+    template: `<data-comp></data-comp>
+              <data-comp></data-comp>`
 })
-export class AppComponent {
-  items: Phone[] = [];
-  constructor(private dataService: DataService){}
-  addItem(name: string, price: number){
-    this.dataService.addData(name, price);
-  }
-  ngOnInit(){
-    this.items = this.dataService.getData();
-  }
-}
+export class AppComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ChildComponent } from './child.component';
-import { BoldDirective } from './bold.directive';
-import { ItalicDirective } from './italic.directive';
-import { TextDecaration } from './text_decaration.directive';
-import { WhileDirective } from './while.directive';
+import { DataComponent } from './data.component';
+import { DataService } from './data.service';
+import { LogService } from './log.service';
 
 @NgModule({
   imports: [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, ChildComponent, BoldDirective, ItalicDirective, TextDecaration, WhileDirective ],
+  declarations: [ AppComponent, DataComponent ],
+  providers: [DataService, LogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
